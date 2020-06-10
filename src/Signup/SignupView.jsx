04ebe8caf4b3f69b6d2922244801e084b050e3e8
Omar Ali <omar.moh.aly@gmail.com>
2020-06-10 07:48:03 +0200

    finished styling the text fields of the signup page

--- src/Signup/SignupView.jsx
+++ src/Signup/SignupView.jsx
@@ -1,37 +1,20 @@
 import React from "react";
 import cookieLogo from "./../resources/images/cookie-logo.png"
+import "./SignupView.css";
 
 function SignupView() {
 
-    const containerStyle = {
-        textAlign: "center"
-    }
 
-    const imageStyle = {
-        height: "300px",
-        width: "300px",
-    };
 
-    const fieldsContainerStyle = {
-        backgroundColor: "#EDEDED",
-        width: "350px",
-        height: "215px",
-        margin: "auto",
-        borderRadius: "10px",
-        // textAlign: "center"
-    }
-
-    const textFieldStyles = {
-        display: "block",
-        width: "90%"
-    }
-
-    return <div style={ containerStyle } >
-            <img style={ imageStyle } src={cookieLogo} alt="cookie-logo"/>
-            <div style={ fieldsContainerStyle }>
-                <input style={ textFieldStyles } type="text" placeholder="First Name"/>
+    return <div className="container">
+            <img className="image" src={cookieLogo} alt="cookie-logo"/>
+            <div className="fields-container">
+                <input type="text" placeholder="First Name"/>
+                <hr/>
                 <input type="text" placeholder="Last Name (Optional)"/>
+                <hr/>
                 <input type="email" placeholder="Email"/>
+                <hr/>
                 <input type="password" placeholder="Password"/>
             </div>
         </div>;
