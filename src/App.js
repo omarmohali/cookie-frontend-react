c212cb7b270c7e1ec00295e5ff5fc456c3dfef4d
Omar Ali <omar.moh.aly@gmail.com>
2020-06-14 08:04:01 +0200

    Setting up routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import SignupView from "./Signup/SignupView";
+import { BrowserRouter } from "react-router-dom";
+import { Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <SignupView />
-    </div>
+    <BrowserRouter>
+        <Route path="/signup" component={SignupView}/>
+    </BrowserRouter>
+
   );
 }
 
