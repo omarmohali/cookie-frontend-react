b2b80aec3e479636c5072104b194fa90539c68ec
Omar Ali <omar.moh.aly@gmail.com>
2020-06-14 10:36:23 +0200

    Added nav bar, still no mobile

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import NavigationBar from "./NavigationBar/NavigationBar";
 import SignupView from "./Signup/SignupView";
 import { BrowserRouter } from "react-router-dom";
 import { Route } from "react-router-dom";
 
+
 function App() {
   return (
     <BrowserRouter>
+        <NavigationBar />
         <Route path="/signup" component={SignupView}/>
     </BrowserRouter>
 
---
+++ src/NavigationBar/NavigationBar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./NavigationBar.css";
+
+
+function NavigationBar() {
+    return <div className={"nav-container"}>
+        <ul className="nav-list">
+            
+            <li className={"nav-list-item"}><a href="">Sign in</a></li>
+            <li className={"nav-list-item"}><a href="">Discover</a></li>
+            <li className={"nav-list-item"}><a href="">Profile</a></li>
+        </ul>
+    </div>
+}
+
+
+export default NavigationBar;
